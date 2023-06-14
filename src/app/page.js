5281bb393cc68bb432887c60ea3a8b281ac64967
Alfredo Sumaran <a.sumaran@gmail.com>
2023-06-13 23:52:30 -0500

    Get nonce and use it to add product to cart

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,58 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import styles from "./page.module.scss";
+
+async function getCart() {
+  const response = await fetch("http://wcpay.test/wp-json/wc/store/v1/cart");
+  const nonce = response.headers.get("nonce");
+  const cart = await response.json();
+  return { nonce, cart };
+}
 
 async function getProducts() {
   const response = await fetch(
-    "http://wcpay.test/wp-json/wc/store/v1/products"
+    "http://wcpay.test/wp-json/wc/store/v1/products",
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
   );
   return await response.json();
 }
 
+async function addProductToCart(product, nonce) {
+  const response = await fetch(
+    "http://wcpay.test/wp-json/wc/store/v1/cart/add-item",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Nonce: nonce,
+      },
+      body: JSON.stringify({ id: product.id, quantity: 1 }),
+    }
+  );
+
+  return await response.json();
+}
+
+const StoreContext = createContext(null);
+
 function ProductItem(props) {
   const { product } = props;
+  const { storeData } = useContext(StoreContext);
 
-  function handleClick() {
-    console.log("add to cart");
+  async function handleClick() {
+    await addProductToCart(product, storeData.nonce);
   }
 
   return (
     <div>
-      <div>{product.name}</div>
+      <div>
+        [{product.id}] - {product.name}
+      </div>
       <div>
         <img width="100" src={product.images[0]?.src} />
       </div>
@@ -30,7 +63,8 @@ function ProductItem(props) {
   );
 }
 
-export default function Home() {
+function Main() {
+  const { storeData, setStoreData } = useContext(StoreContext);
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
@@ -41,6 +75,15 @@ export default function Home() {
 
     return () => {};
   }, []);
+
+  async function onClickGetCartHandler() {
+    const { nonce, cart } = await getCart();
+
+    const newStoreData = storeData;
+    newStoreData.nonce = nonce;
+
+    setStoreData(newStoreData);
+  }
 
   return (
     <div>
@@ -54,6 +97,20 @@ export default function Home() {
           );
         })}
       </ul>
+      <div>
+        <button onClick={onClickGetCartHandler}>Get Cart</button>
+      </div>
     </div>
+  );
+}
+
+export default function Home() {
+  const [storeData, setStoreData] = useState({});
+
+  // we could memoize the value
+  return (
+    <StoreContext.Provider value={{ storeData, setStoreData }}>
+      <Main />
+    </StoreContext.Provider>
   );
 }
