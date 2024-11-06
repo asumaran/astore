968183e0618cf3277f44e53f7ff0507ca2b4a913
Alfredo Sumaran <a.sumaran@gmail.com>
2024-11-06 16:08:18 -0500

    Add ability to checkout the current cart

--- src/app/checkout/page.js
+++ src/app/checkout/page.js
@@ -6,6 +6,46 @@ import CartBlock from "@/components/CartBlock";
 import Navigation from "@/components/Navigation";
 import Debug from "@/components/Debug";
 
+async function doCheckout() {
+  let cartToken = localStorage.getItem("cartToken");
+
+  if (!cartToken) {
+    console.error("No Cart Token found");
+    return;
+  }
+
+  const checkoutData = {
+    billing_address: {
+      email: "asumaran@mail.test",
+      first_name: "Peter",
+      last_name: "Venkman",
+      address_1: "550 Central Park West",
+      city: "New York",
+      state: "NY",
+      postcode: "10023",
+      country: "US",
+    },
+    payment_method: "cod",
+  };
+
+  const response = await fetch(
+    "https://wcpay.test/wp-json/wc/store/v1/checkout",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Cart-Token": cartToken,
+      },
+      body: JSON.stringify(checkoutData),
+    }
+  );
+
+  const cartTokenFromResponse = response.headers.get("Cart-Token");
+  const checkoutResponse = await response.json();
+
+  return { checkout: checkoutResponse, cartToken: cartTokenFromResponse };
+}
+
 async function getCheckoutData() {
   let cartToken = localStorage.getItem("cartToken");
 
@@ -33,18 +73,28 @@ async function getCheckoutData() {
 export default function Checkout() {
   const { cart, cartToken, setCartToken } = useContext(AppContext);
 
+  async function onCheckoutClickHandler() {
+    const { checkout, cartToken } = await doCheckout();
+    console.log("checkout", checkout);
+    setCartToken(cartToken);
+  }
+
   async function getCheckoutDataOnClickHandler() {
     const { checkoutData, cartToken } = await getCheckoutData();
 
     console.log("Checkout Data", checkoutData);
     setCartToken(cartToken);
   }
+
   return (
     <div>
       <Navigation />
       <h1>Checkout Page</h1>
       <CartBlock cart={cart} />
       <hr />
+      <p>
+        <button onClick={onCheckoutClickHandler}>Checkout</button>
+      </p>
       <p>
         <button onClick={getCheckoutDataOnClickHandler}>
           Get Checkout Data
