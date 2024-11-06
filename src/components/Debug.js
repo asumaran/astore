c3fe3067c448957bd9decec0a6f200cb7277839f
Alfredo Sumaran <a.sumaran@gmail.com>
2024-11-06 14:36:29 -0500

    Handle the absence of cart token

--- src/components/Debug.js
+++ src/components/Debug.js
@@ -1,8 +1,11 @@
 export default function Debug({ cartToken }) {
   return (
     <code>
-      Debug: <br />
-      Cart Token: {cartToken.slice(0, 20)}...{cartToken.slice(-20)}
+      Debug <br />
+      Cart Token:{" "}
+      {cartToken
+        ? "Cart Token:" + cartToken.slice(0, 20) + "..." + cartToken.slice(-20)
+        : "No Cart Token"}
     </code>
   );
 }
