475f6b2d0aaaca024a08d97f804219490b553443
Alfredo Sumaran <a.sumaran@gmail.com>
2024-11-06 14:35:39 -0500

    Add ability to remove an item from the cart

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,7 +1,48 @@
 import Currency from "./Currency";
 import styles from "../app/page.module.scss";
+import { AppContext } from "@/app/app-provider";
+import { useContext } from "react";
+
+async function removeItemFromCart(itemKey) {
+  // probably a better idea to move this and get the token from the context or pass it as param.
+  // to avoid having two sources of truth
+  let cartToken = localStorage.getItem("cartToken");
+
+  if (!cartToken) {
+    console.error("No Cart Token found");
+    return;
+  }
+
+  const response = await fetch(
+    "https://wcpay.test/wp-json/wc/store/v1/cart/remove-item",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Cart-Token": cartToken,
+      },
+      body: JSON.stringify({ key: itemKey }),
+    }
+  );
+
+  const cartTokenFromResponse = response.headers.get("Cart-Token");
+  const cart = await response.json();
+
+  return { cart, cartToken: cartTokenFromResponse };
+}
 
 export default function CartItem({ item }) {
+  const { cart, setCart, cartToken, setCartToken } = useContext(AppContext);
+
+  async function onRemoveItemClickHandler() {
+    const { cart, cartToken: cartTokenFromResponse } = await removeItemFromCart(
+      item.key
+    );
+
+    // Update cart and Cart Token
+    setCart(cart);
+    setCartToken(cartTokenFromResponse);
+  }
   return (
     <div className={styles.cartItem}>
       <div className={styles.image}>
@@ -21,6 +62,9 @@ export default function CartItem({ item }) {
       </div>
       <div className={styles.lineTotal}>
         <Currency amount={item.totals.line_total} />
+      </div>
+      <div>
+        <button onClick={onRemoveItemClickHandler}>Remove</button>
       </div>
     </div>
   );
