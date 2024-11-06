284a70574d038902b7437a3a2db201abf8f6ae0b
Alfredo Sumaran <a.sumaran@gmail.com>
2024-11-06 12:52:40 -0500

    Trim the token to fit within the space

--- src/components/Debug.js
+++ src/components/Debug.js
@@ -2,7 +2,7 @@ export default function Debug({ cartToken }) {
   return (
     <code>
       Debug: <br />
-      Cart Token: {cartToken}
+      Cart Token: {cartToken.slice(0, 20)}...{cartToken.slice(-20)}
     </code>
   );
 }
