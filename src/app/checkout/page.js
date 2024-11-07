6cbd2229a0520d2611fc22222b8c5e3cb4a2e652
Alfredo Sumaran <a.sumaran@gmail.com>
2024-11-07 00:19:59 -0500

    Store order params after checkout

--- src/app/checkout/page.js
+++ src/app/checkout/page.js
@@ -1,73 +1,84 @@
-"use client";
+'use client';
 
-import { useContext } from "react";
-import { AppContext } from "../app-provider";
-import CartBlock from "@/components/CartBlock";
-import Navigation from "@/components/Navigation";
-import Debug from "@/components/Debug";
+import { useContext } from 'react';
+import { AppContext } from '../app-provider';
+import CartBlock from '@/components/CartBlock';
+import Navigation from '@/components/Navigation';
+import Debug from '@/components/Debug';
 
 async function doCheckout() {
-  let cartToken = localStorage.getItem("cartToken");
+  let cartToken = localStorage.getItem('cartToken');
 
   if (!cartToken) {
-    console.error("No Cart Token found");
+    console.error('No Cart Token found');
     return;
   }
 
   const checkoutData = {
     billing_address: {
-      email: "asumaran@mail.test",
-      first_name: "Peter",
-      last_name: "Venkman",
-      address_1: "550 Central Park West",
-      city: "New York",
-      state: "NY",
-      postcode: "10023",
-      country: "US",
+      email: 'asumaran@mail.test',
+      first_name: 'Peter',
+      last_name: 'Venkman',
+      address_1: '550 Central Park West',
+      city: 'New York',
+      state: 'NY',
+      postcode: '10023',
+      country: 'US',
     },
-    payment_method: "cod",
+    payment_method: 'cod',
   };
 
   const response = await fetch(
-    "https://wcpay.test/wp-json/wc/store/v1/checkout",
+    'https://wcpay.test/wp-json/wc/store/v1/checkout',
     {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
-        "Cart-Token": cartToken,
+        'Content-Type': 'application/json',
+        'Cart-Token': cartToken,
       },
       body: JSON.stringify(checkoutData),
     }
   );
 
-  const cartTokenFromResponse = response.headers.get("Cart-Token");
+  const cartTokenFromResponse = response.headers.get('Cart-Token');
   const checkoutResponse = await response.json();
 
   return { checkout: checkoutResponse, cartToken: cartTokenFromResponse };
 }
 
 async function getCheckoutData() {
-  let cartToken = localStorage.getItem("cartToken");
+  let cartToken = localStorage.getItem('cartToken');
 
   if (!cartToken) {
-    console.error("No Cart Token found");
+    console.error('No Cart Token found');
     return;
   }
 
   const response = await fetch(
-    "https://wcpay.test/wp-json/wc/store/v1/checkout",
+    'https://wcpay.test/wp-json/wc/store/v1/checkout',
     {
       headers: {
-        "Content-Type": "application/json",
-        "Cart-Token": cartToken,
+        'Content-Type': 'application/json',
+        'Cart-Token': cartToken,
       },
     }
   );
 
-  const cartTokenFromResponse = response.headers.get("Cart-Token");
+  const cartTokenFromResponse = response.headers.get('Cart-Token');
   const checkoutData = await response.json();
 
   return { checkoutData, cartToken: cartTokenFromResponse };
+}
+
+/**
+ * Store order required params in the browser using localStorage.
+ */
+function storeOrderParams(orderId, orderKey, email) {
+  const orders = JSON.parse(localStorage.getItem('orders')) || [];
+
+  orders.push({ orderId, orderKey, email });
+
+  localStorage.setItem('orders', JSON.stringify(orders));
 }
 
 export default function Checkout() {
@@ -75,14 +86,24 @@ export default function Checkout() {
 
   async function onCheckoutClickHandler() {
     const { checkout, cartToken } = await doCheckout();
-    console.log("checkout", checkout);
+
+    // TODO: Do we need to set the cart token anymore after checkout?
     setCartToken(cartToken);
+
+    storeOrderParams(
+      checkout.order_id,
+      checkout.order_key,
+      checkout.billing_address.email
+    );
+
+    // TODO: Clear cart?
+    // TODO: Redirect to home page?
   }
 
   async function getCheckoutDataOnClickHandler() {
     const { checkoutData, cartToken } = await getCheckoutData();
 
-    console.log("Checkout Data", checkoutData);
+    console.log('Checkout Data', checkoutData);
     setCartToken(cartToken);
   }
 
