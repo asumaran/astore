f47253159d8f3d6709dd4614dc8d5e5bcec64fcc
Alfredo Sumaran <a.sumaran@gmail.com>
2024-11-08 00:48:55 -0500

    Add checkout form to provide data manually

--- src/app/checkout/page.js
+++ src/app/checkout/page.js
@@ -8,7 +8,7 @@ import Debug from '@/components/Debug';
 import { useRouter } from 'next/navigation';
 import { cartHasItems } from '@/utils';
 
-async function doCheckout() {
+async function doCheckout(formData) {
   let cartToken = localStorage.getItem('cartToken');
 
   if (!cartToken) {
@@ -18,7 +18,7 @@ async function doCheckout() {
 
   const checkoutData = {
     billing_address: {
-      email: 'asumaran@mail.test',
+      email: formData.get('email'),
       first_name: 'Peter',
       last_name: 'Venkman',
       address_1: '550 Central Park West',
@@ -65,8 +65,12 @@ export default function Checkout() {
   const { cart, setCart, setCartToken, setOrders } = useContext(AppContext);
   const router = useRouter();
 
-  async function onCheckoutClickHandler() {
-    const { checkout } = await doCheckout();
+  async function onFormSubmit(e) {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+
+    const { checkout } = await doCheckout(formData);
 
     const orders = storeOrderParams(
       checkout.order_id,
@@ -85,14 +89,96 @@ export default function Checkout() {
       <Navigation />
       <h1>Checkout Page</h1>
       <CartBlock cart={cart} />
-      <hr />
       {cartHasItems(cart) && (
-        <>
+        <form onSubmit={onFormSubmit}>
           <p>
-            <button onClick={onCheckoutClickHandler}>Checkout</button>
+            <label htmlFor='email'>Email</label>
+            <input type='email' name='email' id='email' />
+          </p>
+          <p>Shipping Address</p>
+          <p>
+            <label htmlFor='shipping-country'>Country</label>
+            <select
+              name='shipping-country'
+              id='shipping-country'
+              autoComplete='country'
+            >
+              <option value='US'>United States</option>
+              <option value='PE'>Perú</option>
+            </select>
+          </p>
+          <p>
+            <label htmlFor='shipping-first-name'>First Name</label>
+            <input
+              type='text'
+              name='shipping-first-name'
+              id='shipping-first-name'
+              autoComplete='given-name'
+            />
+          </p>
+          <p>
+            <label htmlFor='shipping-last-name'>Last Name</label>
+            <input
+              type='text'
+              name='shipping-last-name'
+              id='shipping-last-name'
+              autoComplete='family-name'
+            />
+          </p>
+          <p>
+            <label htmlFor='shipping-address-1'>Address</label>
+            <input
+              type='text'
+              name='shipping-address-1'
+              id='shipping-address-1'
+              autoComplete='address-line1'
+            />
+          </p>
+          <p>
+            <label htmlFor='shipping-address-2'>Apartment, Suite, etc.</label>
+            <input
+              type='text'
+              name='shipping-address-2'
+              id='shipping-address-2'
+              autoComplete='address-line2'
+            />
+          </p>
+          <p>
+            <label htmlFor='shipping-city'>City</label>
+            <input
+              type='text'
+              name='shipping-city'
+              id='shipping-city'
+              autoComplete='address-level2'
+            />
+          </p>
+          <p>
+            <label htmlFor='shipping-state'>State</label>
+            <input
+              type='text'
+              name='shipping-state'
+              id='shipping-state'
+              autoComplete='address-level1'
+            />
+          </p>
+          <p>
+            <label htmlFor='shipping-postcode'>ZIP Code</label>
+            <input
+              type='text'
+              name='shipping-postcode'
+              id='shipping-postcode'
+            />
+          </p>
+          <p>
+            <label htmlFor='phone'>Phone</label>
+            <input type='tel' name='phone' id='phone' autoComplete='tel' />
           </p>
           <hr />
-        </>
+          <p>
+            <button type='submit'>Checkout</button>
+          </p>
+          <hr />
+        </form>
       )}
       <Debug />
     </div>
